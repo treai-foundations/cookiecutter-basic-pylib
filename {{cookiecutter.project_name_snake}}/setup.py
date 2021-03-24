fe746da6b52ceee8f4023b94349687fd597901b7
gekorob <gekorob@users.noreply.github.com>
2021-03-24 23:38:23 +0100

    Fix project name in setup
    Seems that guidelines are telling to use
    kebab case instead of snake case for package name

--- {{cookiecutter.project_name_snake}}/setup.py
+++ {{cookiecutter.project_name_snake}}/setup.py
@@ -6,7 +6,7 @@ with open("README.md") as readme_file:
     readme = readme_file.read()
 
 setup(
-    name="{{ cookiecutter.project_name_snake }}",
+    name="{{ cookiecutter.project_name_kebab }}",
     use_scm_version={
         "write_to": "{{ cookiecutter.project_name_snake }}/_version.py",
         "write_to_template": '__version__ = "{version}"\n',
